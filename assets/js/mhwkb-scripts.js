06d41b84ecebcb90b4f8abff223a0abc0ff3314c
andy5995 <andy400-dev@yahoo.com>
2018-01-11 16:26:23 -0600

    push site build implementing language nav drop-down
    See 774f50cc134419a8b9896fcfa8f467bc6d50fe39 and
    b9fd878e93040000c3fbadec3230f5a084bc61a5

--- assets/js/mhwkb-scripts.js
+++ assets/js/mhwkb-scripts.js
@@ -4,6 +4,20 @@ function checkSearchValAndDropdown() {
     $("#search-container").addClass('open');
   }
   else {
-    $("#search-container").removeClass('open'); 
+    $("#search-container").removeClass('open');
   }
 }
+
+// FOR LANGUAGE DROPDOWN CHANGE DETECTION
+$(document).ready(function(){
+  $('.bfh-selectbox').on('change.bfhselectbox', function () {
+    var lang = $(this).val();
+    if (lang == "en_US") document.location.href = "/category/EN";
+    else if (lang == "de_DE") document.location.href = "/category/DE";
+    else if (lang == "hi_IN") document.location.href = "/category/HI";
+    else if (lang == "ne_NP") document.location.href = "/category/NE";
+    else if (lang == "nl_NL") document.location.href = "/category/NL";
+    else if (lang == "pt_BR") document.location.href = "/category/pt_BR";
+    else if (lang == "sv_SE") document.location.href = "/category/SV";
+  });
+});
