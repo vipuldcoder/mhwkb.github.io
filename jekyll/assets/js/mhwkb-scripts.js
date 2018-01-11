b9fd878e93040000c3fbadec3230f5a084bc61a5
andy5995 <andy400-dev@yahoo.com>
2018-01-11 16:23:58 -0600

    add ne_NP and pt_BR to nav dropdown
    These categories were added after #94 and #102 were submitted

--- jekyll/assets/js/mhwkb-scripts.js
+++ jekyll/assets/js/mhwkb-scripts.js
@@ -4,7 +4,7 @@ function checkSearchValAndDropdown() {
     $("#search-container").addClass('open');
   }
   else {
-    $("#search-container").removeClass('open'); 
+    $("#search-container").removeClass('open');
   }
 }
 
@@ -15,7 +15,9 @@ $(document).ready(function(){
     if (lang == "en_US") document.location.href = "/category/EN";
     else if (lang == "de_DE") document.location.href = "/category/DE";
     else if (lang == "hi_IN") document.location.href = "/category/HI";
+    else if (lang == "ne_NP") document.location.href = "/category/NE";
     else if (lang == "nl_NL") document.location.href = "/category/NL";
+    else if (lang == "pt_BR") document.location.href = "/category/pt_BR";
     else if (lang == "sv_SE") document.location.href = "/category/SV";
   });
 });
